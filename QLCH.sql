a09db70dca03f22bec6f9db1bcf037022816e221
KhanhHuyen <147577946+jkh199@users.noreply.github.com>
2024-04-21 22:53:07 +0700

    Update QLCH.sql theo bao cao

--- QLCH.sql
+++ QLCH.sql
@@ -4,19 +4,19 @@ use QLCH;
 create table CHUCNANG(
 	MACN varchar(10) primary key,
 	TENCN nvarchar(50),
-	TRANGTHAI smallint NOT NULL
+	TRANGTHAI smallint
 )
 
 create table NHOMQUYEN(
 	MAQ varchar(10) primary key,
-	TENQUYEN nvarchar(50),
+	TENQUYEN varchar(50),
 	TRANGTHAI smallint
 )
 
 create table CT_QUYEN(
 	MAQ varchar(10),
 	MACN varchar(10),
-	HANHDONG smallint 
+	HANHDONG nvarchar(50) 
 	primary key (MAQ, MACN),
 	foreign key (MAQ) references NHOMQUYEN(MAQ),
 	foreign key (MACN) references CHUCNANG(MACN)
@@ -24,49 +24,49 @@ create table CT_QUYEN(
 )
 
 create table TAIKHOAN (
-	MATK 		varchar(10) primary key,
+	MATK		varchar(10) primary key,
 	TENDN		varchar(50),
 	MATKHAU		varchar(50),
-	SDT		int,
-	EMAIL 		varchar(50),
+	SDT			int,
+	EMAIL		varchar(50),
 	DIACHI		nvarchar(255),
 	MAQ			varchar(10) foreign key references NHOMQUYEN(MAQ),
-	TRANGTHAI	smallint
+	TRANGTHAI	int
 )
 
 create table LOAISP(
 	MALOAI	varchar(10) primary key,
 	TENLOAI nvarchar(255),
-	TRANGTHAI smallint 
+	TRANGTHAI int 
 )
 
 create table SANPHAM(
 	MASP	varchar(10) primary key,
 	TENSP	nvarchar(255) NOT NULL,
 	MALOAI	varchar(10) foreign key references LOAISP(MALOAI),
-	SOLUONG int,
+	SOLUONG int,			-- soluong tính tự động bằng tổng các số lượng sản phẩm của từng mã chi tiết sản phẩm
 	TRANGTHAI smallint NOT NULL
 )
 
 create table MAUSAC(
 	MAMAU	varchar(10) primary key,
 	TENMAU	nvarchar(50),
-	TRANGTHAI	smallint
+	TRANGTHAI	int
 )
 
 create table SIZE(
 	MASIZE		varchar(10)	primary key,
 	TENSIZE		nvarchar(50),
-	TRANGTHAI	smallint
+	TRANGTHAI	int
 )
 
 create table NCC(
 	MANCC	varchar(10) primary key,
-	TENNCC	nvarchar(50),
+	TENNCC	nvarchar(255),
 	DIACHI	nvarchar(255),
 	EMAIL	varchar(255),
 	SDT		int,
-	TRANGTHAI smallint
+	TRANGTHAI int
 )
 
 create table CT_SANPHAM(
@@ -74,8 +74,8 @@ create table CT_SANPHAM(
 	MASP varchar(10)	foreign key references SANPHAM(MASP),
 	MAMAU varchar(10)	foreign key references MAUSAC(MAMAU),
 	MASIZE varchar(10)	foreign key references SIZE(MASIZE),
-	GIABAN Int,
-	SOLUONG Int,
+	SOLUONG int,
+	GIABAN	int,
 	TRANGTHAI smallint
 )
 
@@ -83,8 +83,8 @@ create table DONHANG(
 	MADH		varchar(10) primary key,
 	MATK		varchar(10)	foreign key references TAIKHOAN(MATK),
 	TONGTIEN	money,
-	PTTT		nvarchar(50),
-	NGAYDH		Date,
+	PTTT		nvarchar(50),		-- "Tiền mặt", "Chuyển khoản"
+	NGAYDH		Datetime,
 	TRANGTHAI	smallint,
 )
 
@@ -103,11 +103,11 @@ create table CT_DONHANG(
 
 create table PHIEUNHAP(
 	MAPHIEU		varchar(10) primary key,
-	NGAYTAO		Date,
+	NGAYTAO		Datetime,
 	MANCC		varchar(10)	foreign key references NCC(MANCC),		
 	MATK		varchar(10)	foreign key references TAIKHOAN(MATK),
-	TONGTIEN	money,
-	TRANGTHAI	smallint,
+	TONGTIEN	int,
+	TRANGTHAI	int,
 )
 
 create table CT_PHIEUNHAP(
@@ -120,7 +120,6 @@ create table CT_PHIEUNHAP(
 	foreign key (MAPHIEU) references PHIEUNHAP(MAPHIEU),
 	foreign key (MACTSP) references CT_SANPHAM(MACTSP)
 )
-
 
 insert into CHUCNANG(MACN, TENCN, TRANGTHAI) values 
 ('khachhang', 'Quản lý khách hàng', 0),
@@ -140,7 +139,7 @@ insert into NHOMQUYEN(MAQ, TENQUYEN, TRANGTHAI) values
 (4, 'admin', 1);
 
 
-/*insert into CT_QUYEN(MAQ, MACN, HANHDONG) values
+insert into CT_QUYEN(MAQ, MACN, HANHDONG) values
 (1, 'nhaphang', 'taomoi'), (1, 'nhaphang', 'xoa'), (1, 'nhaphang', 'capnhat'), (1, 'nhaphang', 'hienthi'), 
 (1, 'sanpham', 'taomoi'), (1, 'sanpham', 'xoa'), (1, 'sanpham', 'capnhat'), (1, 'sanpham', 'hienthi'), 
 (1, 'nhacungcap', 'taomoi'), (1, 'nhacungcap', 'xoa'), (1, 'nhacungcap', 'capnhat'), (1, 'nhacungcap', 'hienthi'), 
@@ -158,7 +157,7 @@ insert into NHOMQUYEN(MAQ, TENQUYEN, TRANGTHAI) values
 (3, 'sanpham', 'hienthi'), (3, 'thuoctinh', 'hienthi'), 
 
 (4, 'nhanvien', 'taomoi'), (4, 'nhanvien', 'xoa'), (4, 'nhanvien', 'capnhat'), (4, 'nhanvien', 'hienthi'), 
-(4, 'nhomquyen', 'taomoi'), (4, 'nhomquyen', 'xoa'), (4, 'nhomquyen', 'capnhat'), (4, 'nhomquyen', 'hienthi');*/
+(4, 'nhomquyen', 'taomoi'), (4, 'nhomquyen', 'xoa'), (4, 'nhomquyen', 'capnhat'), (4, 'nhomquyen', 'hienthi');
 
 
 insert into MAUSAC(MAMAU, TENMAU, TRANGTHAI) values
@@ -171,71 +170,59 @@ insert into SIZE(MASIZE, TENSIZE, TRANGTHAI) values
 insert into LOAISP(MALOAI, TENLOAI, TRANGTHAI) values
 ('01', 'Đầm', 1), ('02', 'Áo', 1), ('03', 'Quần', 1), ('04', 'Chân váy', 1), ('05', 'Đồ bộ', 1), ('06', 'Áo dài', 1);
 
-insert into SANPHAM(MASP, TENSP, MALOAI, GIAVON, GIABAN, TRANGTHAI) values
-('00477', 'Đầm tay phồng cổ trụ', '01', 200000, 389000, 1), 
-('00520', 'Đầm tay ngắn bèo viền ren', '01', 200000, 349000, 1), 
-('00461', 'Đầm tay ngắn giả túi thanh lịch', '01', 200000, 359000, 1), 
-('00472', 'Đầm tay ngắn cổ chữ U', '01', 200000, 349000, 1), 
-('00465', 'Đầm 2 dây nơ', '01', 100000, 289000, 1),
-('00733', 'Áo thun ngắn tay MESIYOU', '02', 100000, 279000, 1),
-('00039', 'Quần tây trơn xẻ tà bên sợi', '03', 200000, 329000, 1), 
-('00147', 'Chân váy ngắn xếp ly xòe', '04', 200000, 249000, 1); 
+insert into SANPHAM(MASP, TENSP, MALOAI, SOLUONG, TRANGTHAI) values
+('S00477', 'Đầm tay phồng cổ trụ', '01', 140, 1), 
+('S00520', 'Đầm tay ngắn bèo viền ren', '01', 60, 1), 
+('S00461', 'Đầm tay ngắn giả túi thanh lịch', '01', 40, 1), 
+('S00472', 'Đầm tay ngắn cổ chữ U', '01', 40, 1), 
+('S00465', 'Đầm 2 dây nơ', '01', 80, 1),
+('S00733', 'Áo thun ngắn tay MESIYOU', '02', 40, 1),
+('S00039', 'Quần tây trơn xẻ tà bên sợi', '03', 40, 1), 
+('S00147', 'Chân váy ngắn xếp ly xòe', '04', 80, 1); 
 --('VDU00448', 'Đầm tay ngắn phối ren hoa kết đá', '', '01', 200000, 389000, 1), 
 --('ATU00733', 'Áo thun ngắn tay MESIYOU', '', '02', 100000, 279000, 1), 
 --('TAT00065', 'Áo thun ngắn tay YOU&ME', '', '02', 100000, 200000, 1), 
 --('SMU00221', 'Sơ mi tay dài cổ bẻ trơn', '', '02', 100000, 249000, 1), 
 --('ASU00691', 'Áo tay loe cột nơ lai ren', '', '02', 100000, 239000, 1), 
 --('ALU00257', 'Áo len sát nách thêu gấu', '', '02', 100000, 239000, 1), 
---('QTR00039', 'Quần tây trơn xẻ tà bên sợi', '', '03', 200000, 329000, 1), 
---('QJB00099', 'Quần jeans dáng baggy xước lai', '', '03', 200000, 300000, 1), 
---('STU00039', 'Quần short tây xếp ly giả váy', '', '03', 80000, 179000, 1), 
---('QKU00016', 'Quần kaki trơn lai thường đơn giản ', '', '03', 200000, 269000, 1), 
---('CVD00100', 'Chân váy dài xẻ bên', '', '04', 200000, 249000, 1), 
---('CVD00104', 'Chân váy dài xanh trơn', '', '04', 200000, 269000, 1), 
---('CVN00147', 'Chân váy ngắn xếp ly xòe', '', '04', 200000, 249000, 1), 
 --('CVN00131', 'Chân váy ngắn chữ A', '', '04', 100000, 219000, 1), 
 --('CVD00086', 'Chân váy dài đai xẻ trước công sở', '', '04', 200000, 319000, 1), 
 --('DBU00050', 'Đồ bộ thỏ Hello Sunday', '', '05', 200000, 0, 1), 
---('DBU00013', 'Đồ bộ xốp áo tay ngắn + quần ngắn kimono', '', '05', 900000, 189000, 1), 
---('DBU00002', 'Đồ bộ áo tay ngắn cổ vest 1 túi + quần ngắn', '', '05', 200000, 0, 1), 
---('ADU00014', 'Áo dài tay phồng hiện đại thướt tha', '', '06', 200000, 349000, 1), 
---('VDT00041', 'Đầm 2 dây trơn tay dài MESIYOU', '', '01', 200000, 300000, 1), 
---('YJU00016', 'Yếm jean túi hộp nút bên', '', '01', 200000, 300000, 1), 
 --('VDU00432', 'Đầm 2 dây bảng to tầng tưa', '', '01', 200000, 269000, 1), 
 --('KVU00025', 'Áo khoác vest tay dài thanh lịch', '', '02', 200000, 400000, 1), 
 --('ALU00256', 'Áo len tay dày dập ly dày dặn', '', '02', 200000, 350000, 1), 
 --('ASU00453', 'Áo kiểu 2 tầng xếp ly', '', '02', 100000, 150000, 1); 
 
 
-insert into CT_SANPHAM(MACTSP, MASP, MAMAU, MASIZE, SOLUONG, TRANGTHAI) values
-('004770104', '00477', '01', '04', 20, 1),
-('004770102', '00477', '01', '02', 10, 1),
-('004770103', '00477', '01', '03', 10, 1),
-('004770202', '00477', '02', '02', 10, 1),
-('004770203', '00477', '02', '03', 20, 1),
-('004770204', '00477', '02', '04', 20, 1),
-('004770302', '00477', '03', '02', 10, 1),
-('004770303', '00477', '03', '03', 20, 1),
-('004770304', '00477', '03', '04', 20, 1),
-('005200101', '00520', '01', '01', 20, 1),
-('005200201', '00520', '02', '01', 20, 1),
-('005200601', '00520', '06', '01', 20, 1),
-('004610201', '00461', '02', '01', 20, 1),
-('004610301', '00461', '03', '01', 20, 1),
-('004720201', '00472', '02', '01', 20, 1),
-('004720501', '00472', '05', '01', 20, 1),
-('004650101', '00465', '01', '01', 20, 1),
-('004650102', '00465', '01', '02', 20, 1),
-('004650201', '00465', '02', '01', 20, 1),
-('004650202', '00465', '02', '02', 20, 1),
-('007330201', '00733', '02', '01', 20, 1),
-('007330301', '00733', '03', '01', 20, 1),
-('000390201', '00039', '02', '01', 20, 1),
-('000390501', '00039', '05', '01', 20, 1),
-('001470101', '00147', '01', '01', 20, 1),
-('001470102', '00147', '01', '02', 20, 1),
-('001470201', '00147', '02', '01', 20, 1),
-('001470202', '00147', '02', '02', 20, 1);
+insert into CT_SANPHAM(MACTSP, MASP, MAMAU, MASIZE, SOLUONG, GIABAN, TRANGTHAI) values
+('S004770104', 'S00477', '01', '04', 20, 200000, 1),
+('S004770102', 'S00477', '01', '02', 10, 200000, 1),
+('S004770103', 'S00477', '01', '03', 10, 200000, 1),
+('S004770202', 'S00477', '02', '02', 10, 200000, 1),
+('S004770203', 'S00477', '02', '03', 20, 100000, 1),
+('S004770204', 'S00477', '02', '04', 20, 400000, 1),
+('S004770302', 'S00477', '03', '02', 10, 300000, 1),
+('S004770303', 'S00477', '03', '03', 20, 150000, 1),
+('S004770304', 'S00477', '03', '04', 20, 120000, 1),
+('S005200101', 'S00520', '01', '01', 20, 140000, 1),
+('S005200201', 'S00520', '02', '01', 20, 160000, 1),
+('S005200601', 'S00520', '06', '01', 20, 180000, 1),
+('S004610201', 'S00461', '02', '01', 20, 200000, 1),
+('S004610301', 'S00461', '03', '01', 20, 200000, 1),
+('S004720201', 'S00472', '02', '01', 20, 200000, 1),
+('S004720501', 'S00472', '05', '01', 20, 200000, 1),
+('S004650101', 'S00465', '01', '01', 20, 200000, 1),
+('S004650102', 'S00465', '01', '02', 20, 200000, 1),
+('S004650201', 'S00465', '02', '01', 20, 300000, 1),
+('S004650202', 'S00465', '02', '02', 20, 350000, 1),
+('S007330201', 'S00733', '02', '01', 20, 200000, 1),
+('S007330301', 'S00733', '03', '01', 20, 150000, 1),
+('S000390201', 'S00039', '02', '01', 20, 100000, 1),
+('S000390501', 'S00039', '05', '01', 20, 200000, 1),
+('S001470101', 'S00147', '01', '01', 20, 100000, 1),
+('S001470102', 'S00147', '01', '02', 20, 300000, 1),
+('S001470201', 'S00147', '02', '01', 20, 400000, 1),
+('S001470202', 'S00147', '02', '02', 20, 200000, 1);
 
 
 
